57c6949cc08b97e99ba853b14c186b58726982ce
Natalia Sorokina <nalya92@list.ru>
2022-05-10 00:05:42 +0300

    feat: create switching languages (partially)

--- src/index.js
+++ src/index.js
@@ -27,3 +27,51 @@ wrapper.append(wrapVirtualKeyboard);
 let virtualKeyboard = document.createElement('div');
 virtualKeyboard.classList.add('virtual-keyboard');
 wrapVirtualKeyboard.append(virtualKeyboard);
+
+
+//массив кнопок по классам
+let keysUp = document.querySelectorAll(".keyUp");
+let keysDown = document.querySelectorAll(".keyDown");
+let keysUpEng = document.querySelectorAll(".keyUpEng");
+let keysDownEng = document.querySelectorAll(".keyDownEng");
+let keysUpRus = document.querySelectorAll(".keyUpRus");
+let keysDownRus = document.querySelectorAll(".keyDownRus");
+let btnSimple = document.querySelectorAll('.simpleBtn');
+let arrBtn = btnSimple.length*2;
+
+
+
+//переключатели
+let CapsLock = '0';
+let Shift = '0';
+let lang = 'en';
+
+
+//переключение языков
+//let language = document.querySelector('html').lang;
+let rusLang = document.getElementsByClassName("rus");
+let engLang = document.getElementsByClassName("eng");
+console.log(rusLang);
+console.log(engLang);
+
+
+if(lang === 'en') {
+    for(let i = 0; i < rusLang.length; i++ ) {
+        if (!document.querySelector(".eng .hidden")) {
+            engLang[i].classList.remove('hidden');
+        }
+        if(!!document.querySelector(".rus .hidden")) {
+            rusLang[i].classList.add('hidden');
+        }
+    }
+
+} else if(lang === 'ru') {
+    for (let i = 0; i < engLang.length; i++) {
+        if (!!document.querySelector(".eng .hidden")) {
+            engLang[i].classList.add('hidden');
+        }
+        if (!document.querySelector(".rus .hidden")) {
+            rusLang[i].classList.remove('hidden');
+        }
+    }
+}
