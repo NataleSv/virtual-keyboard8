1c8add10976beb83317771c5c23af5aaaa956258
Natalia Sorokina <nalya92@list.ru>
2022-05-11 00:31:31 +0300

    fix: fix errors, feat: add language switching

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-
-
-
-
 class FunctionalButtons {
     constructor(value, className, codeKey, typeBtn) {
         this.value = value;
-
         this.className = className;
         this.codeKey = codeKey;
         this.typeBtn = typeBtn;
-
     }
     createFuncButton() {
         let btn = document.createElement('div');
@@ -21,7 +15,6 @@ class FunctionalButtons {
         btn.setAttribute("data-type", this.typeBtn);
         return btn;
     }
-
 }
 
 class SimpleButtons extends FunctionalButtons{
@@ -81,21 +74,13 @@ class SimpleButtons extends FunctionalButtons{
     }
 }
 
-
-
-
 let body = document.body;
 let wrapper = document.createElement('div');
 wrapper.classList.add('wrapper');
 body.append(wrapper);
 
-let warning = document.createElement('form');
-warning.innerHTML = "Дорогой проверяющий, пожалуйста проверьте работу после 10 числа. Постараюсь исправить некоторые моменты в коде и в оформлении. Мой дискорт для связи NataleSv#6370"
-wrapper.append(warning);
-
 let form = document.createElement('form');
 wrapper.append(form);
-
 
 let textarea = document.createElement('textarea');
 textarea.classList.add('textarea');
@@ -109,245 +94,267 @@ let virtualKeyboard = document.createElement('div');
 virtualKeyboard.classList.add('virtual-keyboard');
 wrapVirtualKeyboard.append(virtualKeyboard);
 
+let textDescription = document.createElement("div");
+textDescription.classList.add("text-description");
+wrapper.append(textDescription);
 
-//массив кнопок по классам
-let keysUp = document.querySelectorAll(".keyUp");
-let keysDown = document.querySelectorAll(".keyDown");
-let keysUpEng = document.querySelectorAll(".keyUpEng");
-let keysDownEng = document.querySelectorAll(".keyDownEng");
-let keysUpRus = document.querySelectorAll(".keyUpRus");
-let keysDownRus = document.querySelectorAll(".keyDownRus");
-let btnSimple = document.querySelectorAll('.simpleBtn');
-let arrBtn = btnSimple.length*2;
+let OSInfo = document.createElement('p');
+OSInfo.classList.add("os-info");
+OSInfo.innerHTML = "Клавиатура создана в операционной системе Linux(Ubuntu 20.04.4 LTS)";
+textDescription.append(OSInfo);
+
+let infoLanguageSwitching = document.createElement('p');
+infoLanguageSwitching.classList.add("os-info");
+infoLanguageSwitching.innerHTML = "Для переключения языка комбинация: ShiftLeft + AltLeft";
+textDescription.append(infoLanguageSwitching);
 
 
-
-//переключатели
 let CapsLock = '0';
 let Shift = '0';
 let lang = 'en';
 
 
-//переключение языков
-//let language = document.querySelector('html').lang;
-let rusLang = document.getElementsByClassName("rus");
-let engLang = document.getElementsByClassName("eng");
-console.log(rusLang);
-console.log(engLang);
-
-
-if(lang === 'en') {
-    for(let i = 0; i < rusLang.length; i++ ) {
-        if (!document.querySelector(".eng .hidden")) {
-            engLang[i].classList.remove('hidden');
-        }
-        if(!!document.querySelector(".rus .hidden")) {
-            rusLang[i].classList.add('hidden');
-        }
-    }
-
-} else if(lang === 'ru') {
-    for (let i = 0; i < engLang.length; i++) {
-        if (!!document.querySelector(".eng .hidden")) {
-            engLang[i].classList.add('hidden');
-        }
-        if (!document.querySelector(".rus .hidden")) {
-            rusLang[i].classList.remove('hidden');
-        }
-    }
-}
-
-//добавление кнопок
-
 function addKeys () {
-    for(let i = 0; i < keys.length; i++) {
-        if (keys[i]['type'] === "functional") {
-            let funcBtn = (new FunctionalButtons(keys[i]['value'], keys[i]["btnName"], keys[i]["code"], keys[i]['type'])).createFuncButton();
-            virtualKeyboard.append(funcBtn);
-        }
-        else {
-            let simpleBtn = (new SimpleButtons(keys[i]['value'],keys[i]["valueShift"],keys[i]["valueRu"],keys[i]["valueShiftRu"], keys[i]["btnName"], keys[i]["code"], keys[i]['type'] )).createSimpleButton();
-            virtualKeyboard.append(simpleBtn);
-        }
-    }
+   for(let i = 0; i < keys.length; i++) {
+      if (keys[i]['type'] === "functional") {
+         let funcBtn = (new FunctionalButtons(keys[i]['value'], keys[i]["btnName"], keys[i]["code"], keys[i]['type'])).createFuncButton();
+         virtualKeyboard.append(funcBtn);
+      }
+      else {
+         let simpleBtn = (new SimpleButtons(keys[i]['value'],keys[i]["valueShift"],keys[i]["valueRu"],keys[i]["valueShiftRu"], keys[i]["btnName"], keys[i]["code"], keys[i]['type'] )).createSimpleButton();
+         virtualKeyboard.append(simpleBtn);
+      }
+   }
 }
 addKeys();
 
-//все кнопки
+
+let keysUp = document.querySelectorAll(".keyUp");
+let keysDown = document.querySelectorAll(".keyDown");
+let btnSimple = document.querySelectorAll('.simpleBtn');
+let arrBtn = btnSimple.length*2;
+let rusLang = document.getElementsByClassName("rus");
+let engLang = document.getElementsByClassName("eng");
+
 let btn = document.querySelectorAll('.btn');
 let flag = false;
 
-//события клавиатуры
+
+let keysUpFunc = function keysUpFunc() {
+   for(let i = 0; i < arrBtn; i++) {
+      keysDown[i].classList.add('hidden');
+      keysUp[i].classList.remove('hidden');
+   }
+}
+
+let keysDownFunc = function keysDownFunc() {
+   for(let i = 0; i < arrBtn; i++) {
+      keysUp[i].classList.add('hidden');
+      keysDown[i].classList.remove('hidden');
+   }
+}
+
+let changeEngLang = function changeEngLang() {
+   for (let i = 0; i < rusLang.length; i++) {
+      rusLang[i].classList.add('hidden');
+      engLang[i].classList.remove('hidden');
+   }
+}
+
+let changeRusLang = function changeRusLang() {
+   for (let i = 0; i < engLang.length; i++) {
+      engLang[i].classList.add('hidden');
+      rusLang[i].classList.remove('hidden');
+
+   }
+}
+
+let deleteActiveClass = function deleteActiveClass() {
+   if(document.querySelector('.active')) {
+      let activeBtn = document.querySelector('.active');
+      activeBtn.classList.remove('active');
+   }
+}
+
+let addActiveClass = function addActiveClass(eventCode) {
+   if (document.querySelector('#'+eventCode)){
+      let activeBtn = document.querySelector('#'+eventCode);
+      activeBtn.classList.add('active');
+   }
+}
+
+
 document.addEventListener('keydown', function(event) {
-    let activeBtn = document.querySelector('#'+event.code);
-    activeBtn.classList.add('active');
-    textarea.focus();
+   addActiveClass(event.code);
+   textarea.focus();
 
-    if(event.shiftKey === true && flag === false) {
-        Shift = '1';
-        for(let i = 0; i < arrBtn; i++) {
-            keysDown[i].classList.add('hidden');
-            keysUp[i].classList.remove('hidden');
-        }
-    }
+   if(event.shiftKey === true) {
+         Shift = '1';
+         keysUpFunc();
+   }
 
-    if(event.code === "AltLeft") flag = true;
-    if(event.code === "ShiftLeft" && flag) {
+   if(event.code === "AltLeft") flag = true;
+   if(event.code === "ShiftLeft" && flag) {
+      flag = false;
 
-        flag = false;
-        if(lang === 'en') {
-            lang = 'ru';
+      if(lang === 'en') {
+         lang = 'ru';
+         changeRusLang();
 
-        } else if (lang === 'ru') {
-            lang = 'en';
-        }
+      } else if (lang === 'ru') {
+         lang = 'en';
+        changeEngLang();
+      }
+   }
 
-        alert(lang);
-    }
-
-    if(event.code === "CapsLock") {
-        if (CapsLock === "0") {
-
+   if(event.code === "CapsLock") {
+         if (CapsLock === "0") {
             CapsLock = "1";
-        } else if(CapsLock === "1") {
+         } else if(CapsLock === "1") {
             CapsLock = "0";
-        }
-    }
+         }
+      }
 });
 
 
 document.addEventListener('keyup', function(event) {
+   deleteActiveClass();
 
-    let activeBtn = document.querySelector('#'+event.code);
-    activeBtn.classList.remove('active');
-
-    if(event.shiftKey === false) {
-        Shift = '0';
-        for(let i = 0; i < arrBtn; i++) {
-            keysUp[i].classList.add('hidden');
-            keysDown[i].classList.remove('hidden');
-        }
-    }
+   if(event.shiftKey === false) {
+      Shift = '0';
+      keysDownFunc();
+   }
 });
 
-
-//события мыши
 document.addEventListener('mousedown', function (event) {
-    let entryField = document.querySelector("textarea").value;
-    let activeBtn = document.querySelector('#' + event.target.dataset.code);
-    activeBtn.classList.add('active');
+   addActiveClass(event.target.dataset.code);
+},true);
 
-    //functional keys
-    //BackSpace
-    if (event.target.dataset.code === "Backspace") {
 
-        let startCursor = document.querySelector("textarea").selectionStart;
-        let endCursor = document.querySelector("textarea").selectionEnd;
-        let textareaLeng = document.querySelector("textarea").value.length;
+document.addEventListener('mousedown', function (event) {
 
-            document.querySelector("textarea").value = entryField.slice(0, startCursor - 1);
-
-    }
-
-    //CapsLock
-    if (event.target.dataset.code === "CapsLock") {
-
-        if (CapsLock === "0") {
-            CapsLock = "1";
-        } else if (CapsLock === "1") {
-            CapsLock = "0";
-        }
-    }
-
-    //Shift
-    if (event.target.dataset.code === "ShiftLeft" ||
-        event.target.dataset.code === "ShiftRight") {
-        console.log(event.target.dataset.code);
-
-        for (let i = 0; i < arrBtn; i++) {
-            keysDown[i].classList.add('hidden');
-            keysUp[i].classList.remove('hidden');
-        }
-        if (Shift === '0') {
-            Shift = '1';
-        } else if (Shift === '1') {
+   let entryField = document.querySelector("textarea").value;
+   let addValueTextBtn = function addText(firstValue, secondValue, i) {
+      if(CapsLock === '0') {
+         if(Shift ==='0') {
+            document.querySelector("textarea").value = entryField + keys[i][firstValue];
+         } else if (Shift === '1') {
             Shift = '0';
-        }
-    }
+            document.querySelector("textarea").value = entryField + keys[i][secondValue];
+         }
+      } else if (CapsLock === '1') {
+         if(Shift === '0') {
+            document.querySelector("textarea").value = entryField + keys[i][secondValue];
+         } else if (Shift === '1') {
+            Shift = '0';
+            document.querySelector("textarea").value = entryField + keys[i][firstValue];
+         }
+      }
+   }
 
-    //Space
-    if (event.target.dataset.code === "Space") {
-        document.querySelector("textarea").value = entryField + " ";
-    }
-
-    //Enter
-    if (event.target.dataset.code === "Enter") {
-        document.querySelector("textarea").value = entryField + "\n";
-    }
-
-    //Tab
-    if (event.target.dataset.code === "Tab") {
-        document.querySelector("textarea").value = entryField + "    ";
-    }
-
-    //Delete
+   let addValueNumberBtn = function addValueNumberBtn(firstValue, secondValue, i) {
+      if (Shift === '0') {
+         document.querySelector("textarea").value = entryField + keys[i][firstValue];
+      } else if (Shift === '1') {
+         Shift = '0';
+         document.querySelector("textarea").value = entryField + keys[i][secondValue];
+      }
+   }
 
 
-    //letter keys
-    if(event.target.dataset.type === "letter") {
+   if (event.target.dataset.code === "Backspace") {
+      let startCursor = document.querySelector("textarea").selectionStart;
+      document.querySelector("textarea").value = entryField.slice(0, startCursor - 1);
+   }
 
-        for(let i = 0; i < keys.length; i++) {
+   if (event.target.dataset.code === "CapsLock") {
+      if (CapsLock === "0") {
+         CapsLock = "1";
+         keysUpFunc();
 
-            if (event.target.dataset.code === keys[i]['code']) {
+      } else if (CapsLock === "1") {
+         CapsLock = "0";
+         keysDownFunc();
+      }
+   }
 
-                if(CapsLock === '0') {
-                    if(Shift ==='0') {
-                        document.querySelector("textarea").value = entryField + keys[i]['value'];
-                    } else if (Shift === '1') {
-                        Shift = '0';
-                        document.querySelector("textarea").value = entryField + keys[i]['valueShift'];
-                    }
-                } else if (CapsLock === '1') {
-                    if(Shift === '0') {
-                        document.querySelector("textarea").value = entryField + keys[i]['valueShift'];
-                    } else if (Shift === '1') {
-                        Shift = '0';
-                        document.querySelector("textarea").value = entryField + keys[i]['value'];
-                    }
-                }
+   if (event.target.dataset.code === "ShiftLeft" ||
+       event.target.dataset.code === "ShiftRight") {
+      console.log(event.target.dataset.code);
+      keysUpFunc();
+
+      if (Shift === '0') {
+         Shift = '1';
+      } else if (Shift === '1') {
+         Shift = '0';
+      }
+   }
+
+   if (event.target.dataset.code === "Space") {
+      document.querySelector("textarea").value = entryField + " ";
+   }
+
+   if (event.target.dataset.code === "Enter") {
+      document.querySelector("textarea").value = entryField + "\n";
+   }
+
+   if (event.target.dataset.code === "Tab") {
+      document.querySelector("textarea").value = entryField + "    ";
+   }
+
+   if (event.target.dataset.code === "Delete") {
+      document.querySelector("textarea").value = "";
+   }
+
+   if(event.target.dataset.code === "AltLeft") flag = true;
+   if(event.target.dataset.code === "ShiftLeft" && flag) {
+      flag = false;
+      if(lang === 'en') {
+         lang = 'ru';
+         changeRusLang();
+
+      } else if (lang === 'ru') {
+         lang = 'en';
+         changeEngLang();
+      }
+   }
+
+   if(event.target.dataset.type === "letter") {
+      for(let i = 0; i < keys.length; i++) {
+         if (event.target.dataset.code === keys[i]['code']) {
+            if(lang === 'en') {
+               addValueTextBtn('value', 'valueShift', i);
+            } else if (lang === 'ru') {
+               addValueTextBtn('valueRu', 'valueShiftRu', i);
             }
-        }
+         }
+      }
+   }
 
-    }
 
-    //number keys
-    if(event.target.dataset.type === "number") {
-        for(let i = 0; i < keys.length; i++) {
-            if (event.target.dataset.code === keys[i]['code']) {
-                if (Shift === '0') {
-                    document.querySelector("textarea").value = entryField + keys[i]['value'];
-                } else if (Shift === '1') {
-                    Shift = '0';
-                    document.querySelector("textarea").value = entryField + keys[i]['valueShift'];
-                }
+   if(event.target.dataset.type === "number") {
+      for(let i = 0; i < keys.length; i++) {
+         if (event.target.dataset.code === keys[i]['code']) {
+            if(lang === 'en') {
+               addValueNumberBtn('value', 'valueShift', i);
+         } else if (lang === 'ru') {
+               addValueNumberBtn('valueRu', 'valueShiftRu', i);
             }
-        }
-    }
-
+         }
+      }
+   }
 });
+
 
 document.addEventListener('mouseup', function (event) {
-    let activeBtn = document.querySelector('.active');
-    activeBtn.classList.remove('active');
 
-
-    if (event.target.dataset.code === "ShiftLeft" ||
-        event.target.dataset.code === "ShiftRight") {
-
-        for (let i = 0; i < arrBtn; i++) {
-            keysUp[i].classList.add('hidden');
-            keysDown[i].classList.remove('hidden');
-        }
-    }
+   deleteActiveClass();
+   if (event.target.dataset.code === "ShiftLeft" ||
+       event.target.dataset.code === "ShiftRight") {
+      keysDownFunc();
+   }
 });
+
+
+
 
