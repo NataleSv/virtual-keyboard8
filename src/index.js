9bac26cce51ba50f1894fe79976e52fd11d62373
Natalia Sorokina <nalya92@list.ru>
2023-05-01 21:59:52 +0300

    refactor: move classes to separate files

--- src/index.js
+++ src/index.js
@@ -1,79 +1,4 @@
-class FunctionalButtons {
-    constructor(value, className, codeKey, typeBtn) {
-        this.value = value;
-        this.className = className;
-        this.codeKey = codeKey;
-        this.typeBtn = typeBtn;
-    }
-    createFuncButton() {
-        let btn = document.createElement('div');
-        btn.classList.add('btn');
-        btn.classList.add(this.className);
-        btn.innerHTML = this.value;
-        btn.id = this.codeKey;
-        btn.setAttribute("data-code", this.codeKey);
-        btn.setAttribute("data-type", this.typeBtn);
-        return btn;
-    }
-}
-
-class SimpleButtons extends FunctionalButtons{
-    constructor(value,valueShift,valueRu,valueShiftRu, className, codeKey, typeBtn, ) {
-        super(value, className, codeKey, typeBtn);
-        this.valueShift = valueShift;
-        this.valueRu = valueRu;
-        this.valueShiftRu = valueShiftRu;
-
-    }
-    createSimpleButton() {
-        let btn = document.createElement('div');
-        btn.classList.add('btn');
-        btn.classList.add('simpleBtn')
-        btn.classList.add(this.className);
-
-        let engBtn = document.createElement('span');
-        engBtn.classList.add('eng');
-
-        btn.append(engBtn);
-
-        let rusBtn = document.createElement('span');
-        rusBtn.classList.add('rus');
-        rusBtn.classList.add('hidden');
-        btn.append(rusBtn);
-
-        let engKeydown = document.createElement('span');
-        engKeydown.classList.add('keyDown');
-        engKeydown.classList.add('keyDownEng');
-        engKeydown.innerHTML = this.value;
-        engBtn.append(engKeydown);
-
-        let engKeyUp = document.createElement('span');
-        engKeyUp.classList.add('keyUp');
-        engKeyUp.classList.add('keyUpEng');
-        engKeyUp.innerHTML = this.valueShift;
-        engKeyUp.classList.add('hidden');
-        engBtn.append(engKeyUp);
-
-        let rusKeydown = document.createElement('span');
-        rusKeydown.classList.add('keyDown');
-        rusKeydown.classList.add('keyDownRus');
-        rusKeydown.innerHTML = this.valueRu;
-        rusBtn.append(rusKeydown);
-
-        let rusKeyUp = document.createElement('span');
-        rusKeyUp.classList.add('keyUp');
-        rusKeyUp.classList.add('keyUpRus');
-        rusKeyUp.innerHTML = this.valueShiftRu;
-        rusKeyUp.classList.add('hidden');
-        rusBtn.append(rusKeyUp);
-
-        btn.id = this.codeKey;
-        btn.setAttribute("data-code", this.codeKey);
-        btn.setAttribute("data-type", this.typeBtn);
-        return btn;
-    }
-}
-
+"use strict";
 let body = document.body;
 let wrapper = document.createElement('div');
 wrapper.classList.add('wrapper');
@@ -127,6 +52,8 @@ function addKeys () {
    }
 }
 addKeys();
+
+
 
 
 let keysUp = document.querySelectorAll(".keyUp");
